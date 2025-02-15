908ffb06cd09d0c05b71c2c169565e117689b8eb
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-15 10:07:51 +0530

    Formact

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use(cors());
 
 //Create Server
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 //Create Responces
-app.get("/",(req,res)=>{
-    res.send("Hello Node");
-})
+app.get("/", (req, res) => {
+  res.send("Hello Node");
+});
