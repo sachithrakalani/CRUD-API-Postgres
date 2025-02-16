64b3ff459553ec1cff2c0cfdcf59cba9bdd29754
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-16 13:04:21 +0530

    Create user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,7 +6,14 @@ export const getAllUsers = async () => {
 };
 
 export const getUserById = async (id) => {
-    const result = await pool.query("SELECT * FROM user WHERE id = $1", [id]);
-    return result.rows[0];
-  };
-  
+  const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  return result.rows[0];
+};
+
+export const createUser = async (name, email) => {
+  const result = await pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING * ",
+    [name, email]
+  );
+  return result.rows[0];
+};
