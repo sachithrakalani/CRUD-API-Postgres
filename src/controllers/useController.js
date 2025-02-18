76620241cdc3cbe16ee6832a38257b98bf64246f
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-18 10:34:08 +0530

    Create user controller

---
+++ src/controllers/useController.js
@@ -0,0 +1,19 @@
+import { createUserService } from "../models/userModel.js";
+
+const handleResponse = (res, status, message, data = null) => {
+  res.status(status).json({
+    status,
+    message,
+    data,
+  });
+};
+
+export const createUser = async (req, res, next) => {
+  const { name, email } = req.body;
+  try {
+    const newUser = await createUserService(name, email);
+    handleResponse(res, 201, "User Created Successfuly", newUser);
+  } catch (error) {
+    next(error);
+  }
+};
